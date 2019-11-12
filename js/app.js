dfdb65dcbc1e65515e5272ae493ba5eaf02f9d10
Thiago Costa <thiguet2014@gmail.com>
2019-11-12 09:05:23 -0300

    Menu responsivo. Botão de Calcular Gastos criado. Falta apenas realizar o calculo e devolver o feedback.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
 var backgroundColors = {
     "#calc-agua": "#4BD7FA",
-    "#calc-energia": "#F0D838"
+    "#calc-energia": "#F0D838",
+    "#about": "#ACFA69"
 };
 var fontColors = {
     "#calc-agua": "white",
-    "#calc-energia": "rgb(119, 67, 67)"
+    "#calc-energia": "rgb(119, 67, 67)",
+    "#about": "white"
+};
+var headings = {
+    "#calc-agua": "Calculadora de Água",
+    "#calc-energia": "Calculadora de Energia",
+    "#about": "EcoMoney"
 };
 var messages = {
     "fa-user-plus": "Quantas pessoas vivem na mesma casa que você ?",
@@ -16,10 +23,13 @@ var messages = {
 $(document).ready(() => {
     $(".container > section").hide();
     $("#calc-agua").fadeIn();
-    $("#question-container").hide();
+    $("#title").html(headings['#calc-agua']);
+    $("#question-container").removeClass('show');
 
     $(".menu > li").on("click", (evt) => {
         var hash = $(evt.currentTarget).children()[0].hash;
+        
+        $("#title").html(headings[hash]);
         $(".container > section").hide();
         $(hash).fadeIn();
         $('body').css('background-color', backgroundColors[hash]);
@@ -28,13 +38,22 @@ $(document).ready(() => {
 
     $(".water-spent-icons").on("click", (evt) => {
         // CSS Stuff
-        $(".water-spent-icons").removeClass("active");
-        $(evt.currentTarget).addClass("active");
+        if($(evt.currentTarget).hasClass("active")) {
+            $("#question-text").html('');
+            $("#question-container").removeClass('show');
+            $(evt.currentTarget).removeClass("active");
+        } else { 
+            $("#question-text").html(messages[evt.currentTarget.id]);
+            $("#question-container").addClass('show');
+            $(evt.currentTarget).addClass("active");
+        }
         
         // JS Actions
-        $("#question-text").html(messages[evt.currentTarget.id]);
+        if($("#question-container").hasClass("show") && !$(".water-spent-icons").hasClass('active')) {
+        } else {
+        }
+
         $("#counter").val(0);
-        $("#question-container").fadeIn();
     });
 
     $("#fa-plus").on("click", () => {
@@ -44,5 +63,12 @@ $(document).ready(() => {
     $("#fa-minus").on("click", () => {
         const value = parseInt($("#counter").val());
         $("#counter").val( value === 0 ? 0 : value - 1);
+    });
+
+    $("#collapse").on("click", () => {
+        if($(".menu").hasClass("show"))
+            $(".menu").removeClass('show');
+        else 
+            $(".menu").addClass('show');
     });
 });
