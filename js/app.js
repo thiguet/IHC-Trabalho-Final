d5c1eaece15bc58151a26e11d91abe95318dddf6
Thiago Costa <thiguet2014@gmail.com>
2019-11-12 00:01:37 -0300

    Consertado o clique de uma imagem para a outra. Dessa forma, o texto muda na aba da calculadora de água, assim como o foco também. A utilizaçãode ícones e fontes remete à um ambiente educativo.

--- js/app.js
+++ js/app.js
@@ -6,14 +6,43 @@ var fontColors = {
     "#calc-agua": "white",
     "#calc-energia": "rgb(119, 67, 67)"
 };
+var messages = {
+    "fa-user-plus": "Quantas pessoas vivem na mesma casa que você ?",
+    "fa-toilet": "Quantas descargas você utiliza em média por dia ?",
+    "fa-shower": "Quantos minutos você gasta no chuveiro por vez ?",
+    "fa-tshirt": "Quantas vezes você lava a roupa na semana ?"
+};
 
 $(document).ready(() => {
     $(".container > section").hide();
     $("#calc-agua").fadeIn();
-    $(".menu > li > a").on("click", (evt) => {
+    $("#question-container").hide();
+
+    $(".menu > li").on("click", (evt) => {
+        var hash = $(evt.currentTarget).children()[0].hash;
         $(".container > section").hide();
-        $(evt.currentTarget.hash).fadeIn();
-        $('body').css('background-color', backgroundColors[evt.currentTarget.hash]);
-        $('body').css('color', fontColors[evt.currentTarget.hash]);
+        $(hash).fadeIn();
+        $('body').css('background-color', backgroundColors[hash]);
+        $('body').css('color', fontColors[hash]);
+    });
+
+    $(".water-spent-icons").on("click", (evt) => {
+        // CSS Stuff
+        $(".water-spent-icons").removeClass("active");
+        $(evt.currentTarget).addClass("active");
+        
+        // JS Actions
+        $("#question-text").html(messages[evt.currentTarget.id]);
+        $("#counter").val(0);
+        $("#question-container").fadeIn();
+    });
+
+    $("#fa-plus").on("click", () => {
+        $("#counter").val(parseInt($("#counter").val()) + 1);
+    });
+
+    $("#fa-minus").on("click", () => {
+        const value = parseInt($("#counter").val());
+        $("#counter").val( value === 0 ? 0 : value - 1);
     });
 });
