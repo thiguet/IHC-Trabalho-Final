9b31ceed2d5985f1cbc3b6d1cb82ab928f4060e1
Thiago Costa <thiguet2014@gmail.com>
2019-11-11 21:01:02 -0300

    Making it get a little more beautiful.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,19 @@
+var backgroundColors = {
+    "#calc-agua": "#4BD7FA",
+    "#calc-energia": "#F0D838"
+};
+var fontColors = {
+    "#calc-agua": "white",
+    "#calc-energia": "rgb(119, 67, 67)"
+};
+
 $(document).ready(() => {
-    $(".container > section").css('display', 'none');
-    $("#calc-agua").css('display', 'flex');
+    $(".container > section").hide();
+    $("#calc-agua").fadeIn();
     $(".menu > li > a").on("click", (evt) => {
-        $(".container > section").css('display', 'none');
-        $(evt.currentTarget.hash).css('display', 'flex');
+        $(".container > section").hide();
+        $(evt.currentTarget.hash).fadeIn();
+        $('body').css('background-color', backgroundColors[evt.currentTarget.hash]);
+        $('body').css('color', fontColors[evt.currentTarget.hash]);
     });
 });
